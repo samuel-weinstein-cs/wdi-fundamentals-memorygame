46cda00ba0451b372a7616d20f34efb7870ee1c1
Sam Weinstein <raeweinst@gmail.com>
2019-10-07 10:03:50 -0400

    cards now flip over

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,8 +21,7 @@ let cards=[
 	}
 ];
 let cardsInPlay=[];
-let cardOne=0;
-let cardTwo=2;
+
 function checkForMatch(){
 	if(cardsInPlay.length===2){
 		if(cardsInPlay[0]==cardsInPlay[1]){
@@ -32,12 +31,23 @@ function checkForMatch(){
 		}
 	}
 }
-function flipCard(cardId){
+function flipCard(){
+	var cardId=this.getAttribute("data-id");
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped the "+cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
+	this.setAttribute("src",cards[cardId].cardImage);
 	checkForMatch();
 }
-flipCard(cardOne);
-flipCard(cardTwo);
+function createBoard(){
+	for(var i=0; i<cards.length; i++){
+		var cardElement=document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
