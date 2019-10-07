8ff2988248575978295c879025d24f8298b6ac3e
Sam Weinstein <raeweinst@gmail.com>
2019-10-07 05:43:01 -0400

    updated game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,20 @@
 let cards=["queen","queen","king","king"];
 let cardsInPlay=[];
-let cardOne=cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped the "+cardOne);
-let cardTwo=cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped the "+cardTwo);
-if(cardsInPlay.length===2){
-	if(cardsInPlay[0]==cardsInPlay[1]){
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
+let cardOne=0;
+let cardTwo=2;
+function checkForMatch(){
+	if(cardsInPlay.length===2){
+		if(cardsInPlay[0]==cardsInPlay[1]){
+			console.log("You found a match!");
+		} else {
+			console.log("Sorry, try again.");
+		}
 	}
 }
+function flipCard(cardId){
+	console.log("User flipped the "+cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+flipCard(cardOne);
+flipCard(cardTwo);
