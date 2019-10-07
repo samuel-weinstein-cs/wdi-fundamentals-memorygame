5fcddac59cfcffae8b7995d2b01844f921b9aede
Sam Weinstein <raeweinst@gmail.com>
2019-10-07 04:57:45 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,15 @@
-var cardOne="queen";
-var cardTwo="king";
-var cardThree="queen";
-var cardFour="king";
-console.log("User flipped "+ cardOne);
-console.log("User flipped "+ cardTwo);
+let cards=["queen","queen","king","king"];
+let cardsInPlay=[];
+let cardOne=cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped the "+cardOne);
+let cardTwo=cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped the "+cardTwo);
+if(cardsInPlay.length===2){
+	if(cardsInPlay[0]==cardsInPlay[1]){
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
